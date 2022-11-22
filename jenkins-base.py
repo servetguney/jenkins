00e1cc18e5f19785943caaa4cce6273b5b21a3ed
servetguney <servetguney@gmail.com>
2022-11-22 16:08:23 +0300

    asas

--- jenkins-base.py
+++ jenkins-base.py
@@ -1,1 +1,15 @@
 # The initial
+
+import jenkins
+
+server = jenkins.Jenkins('http://172.16.16.20:8080', username='admin', password='admin')
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+jobs = server.get_jobs()
+print(jobs)
+for i in jobs:
+    print(i['name']," ",i['url'])
+    configuration=server.get_job_config(i['name'])
+    print(configuration)
